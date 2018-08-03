00e8653fe9c6087195e781a734c50070ae6c8df9
Jude Dike <dikejude49@gmail.com>
2018-08-03 22:19:32 +0100

    keys

--- startnode.sh
+++ startnode.sh
@@ -1,1 +1,1 @@
-geth --networkid 4224 --mine --minerthreads 1 --datadir "~/judedike/private" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password ~/judedike/private/password.sec --ipcpath "~/Library/Ethereum/geth.ipc"
+geth --networkid 4224 --mine --minerthreads 1 --datadir "~/blockchain/private" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password ~/blockchain/private/password.sec --ipcpath "~/Library/Ethereum/geth.ipc"
